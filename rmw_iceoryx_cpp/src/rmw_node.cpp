f2737117827d78411e33a20ebdc51b284b7b99d8
Karsten Knese <karsten.knese@us.bosch.com>
2020-07-13 17:54:01 -0700

    new rmw_create_node API
    Signed-off-by: Karsten Knese <karsten.knese@us.bosch.com>

--- rmw_iceoryx_cpp/src/rmw_node.cpp
+++ rmw_iceoryx_cpp/src/rmw_node.cpp
@@ -26,17 +26,11 @@ rmw_node_t *
 rmw_create_node(
   rmw_context_t * context,
   const char * name,
-  const char * namespace_,
-  size_t domain_id,
-  bool localhost_only)
+  const char * namespace_)
 {
-  (void)domain_id;
-  (void)localhost_only;  // funny ;)
-
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(context, nullptr);
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(name, nullptr);
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(namespace_, nullptr);
-
 
   RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
     rmw_create_node

43cdb2e08b4c66ba42b096cd0e0132a32db1a66f
Simon Hoinkis <simon.hoinkis@apex.ai>
2022-01-11 15:55:30 +0100

    Add gid support (#72)
    * Implement rmw_compare_gids_equal()
    
    Signed-off-by: Simon Hoinkis <simon.hoinkis@apex.ai>
    
    * Use UniquePortId from iceoryx
    
    Signed-off-by: Simon Hoinkis <simon.hoinkis@apex.ai>
    
    * Remove typo in function name and simplify array passing
    
    Signed-off-by: Simon Hoinkis <simon.hoinkis@apex.ai>

--- rmw_iceoryx_cpp/src/rmw_compare_guids_equal.cpp
+++ rmw_iceoryx_cpp/src/rmw_compare_guids_equal.cpp
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "rcutils/error_handling.h"
+#include <cstring>
 
+#include "rcutils/error_handling.h"
 #include "rmw/impl/cpp/macros.hpp"
 #include "rmw/rmw.h"
+
 
 extern "C"
 {
@@ -27,10 +29,22 @@ rmw_compare_gids_equal(
 {
   *result = false;
 
-  RCUTILS_CHECK_ARGUMENT_FOR_NULL(gid1, RMW_RET_ERROR);
-  RCUTILS_CHECK_ARGUMENT_FOR_NULL(gid2, RMW_RET_ERROR);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(gid1, RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+    gid1,
+    gid1->implementation_identifier,
+    rmw_get_implementation_identifier(),
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(gid2, RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+    gid2,
+    gid2->implementation_identifier,
+    rmw_get_implementation_identifier(),
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
 
-  RMW_SET_ERROR_MSG("rmw_iceoryx_cpp does not support gids.");
-  return RMW_RET_UNSUPPORTED;
+  if (std::memcmp(gid1->data, gid2->data, RMW_GID_STORAGE_SIZE) == 0) {
+    *result = true;
+  }
+  return RMW_RET_OK;
 }
 }  // extern "C"
